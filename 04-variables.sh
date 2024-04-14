bf73a7da364063ccb96cfedc719f875c7e3d9ba5
Girish Raja <you@example.com>
2024-04-15 00:31:20 +0530

    added 2nd script

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hello $PERSON2 bad morning"
+echo "$PERSON2:: Hello $PERSON1 very bad morning"
+echo "$PERSON1:: How are you  $PERSON2?"
+echo "$PERSON2:: what about u r error, fuck u"
