6d9d8bb58e86470704e2e3ed2b9d43fe46c070a7
Girish Raja <you@example.com>
2024-04-15 00:04:28 +0530

    added 2nd script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Shakuntala:: Hello yashwanth bad morning"
-echo "Yaswanth:: Hello shakuntala very bad morning"
-echo "Shakuntala:: How are you  yashwanth?"
-echo "Yaswanth:: what about u r error, fuck u"
+PERSON1=Shakuntala
+PERSON2=Yaswanth
+
+echo "$PERSON1:: Hello $PERSON2 bad morning"
+echo "$PERSON2:: Hello $PERSON1 very bad morning"
+echo "$PERSON1:: How are you  $PERSON2?"
+echo "$PERSON2:: what about u r error, fuck u"
