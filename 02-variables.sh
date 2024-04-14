919e4e5d5224bb3722ca5f7d36d1a99988bdf79b
Girish Raja <you@example.com>
2024-04-14 23:48:18 +0530

    added 2nd script

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Shakuntala:: Hello yashwanth bad morning"
+echo "Yaswanth:: Hello shakuntala very bad morning"
+echo "Shakuntala:: How are you  yashwanth?"
+echo "Yaswanth:: what about u r error, fuck u"
