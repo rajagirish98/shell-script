a3daa2d4b26fb98c208105dd7100cd5bf05ef3a1
Girish Raja <you@example.com>
2024-04-16 18:46:24 +0530

    added 2nd script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -2,12 +2,12 @@
 
 FRUITS=("Apple" "Bannana" "Mango" "Kiwi")
 
-echo "First value: ${FRUITS(0)}"
+echo "First value: ${FRUITS[0]}"
 
-echo "Second value: ${FRUITS(1)}"
+echo "Second value: ${FRUITS[1]}"
 
-echo "Second value: ${FRUITS(2)}"
+echo "Second value: ${FRUITS[2]}"
 
-echo "Third value: ${FRUITS(3)}"
+echo "Third value: ${FRUITS[3]}"
 
-echo "All Fruits: ${FRUITS(@)}"
+echo "All Fruits: ${FRUITS[@]}"
