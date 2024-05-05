e595b9d4cea54033f80425abfe6213e41d66d41b
Girish Raja <you@example.com>
2024-05-05 22:33:06 +0530

    added 2nd script

--- bull-scripting/01-data-types.sh
+++ bull-scripting/01-data-types.sh
@@ -2,9 +2,9 @@
 
 FRUITS=("Kiwi" "Apple" "Banana" "Grapes")
 
-echo "First value {$FRUITS [$1]}"
+echo "First value {FRUITS [$1]}"
 
-echo "Second value {$FRUITS2 [$2]}"
+echo "Second value {$FRUITS [$2]}"
 
 echo "Third value {$FRUITS [$3]}"
 
