a32bd90edcc098777f3605f4b61446673fc74fa6
Girish Raja <you@example.com>
2024-05-06 02:08:18 +0530

    added 2nd script

--- bull-scripting/04-validate.sh
+++ bull-scripting/04-validate.sh
@@ -2,17 +2,6 @@
 
 ID=$(id -u)
 
-VALIDATE(){
-
-if [ $1 -ne 0 ]
-
-then
-    echo "$2.... Failure"
-else
-    echo "$2.... Success $"
-fi
-}
-
 
 if [ $ID -ne 0 ]
 then
@@ -22,7 +11,4 @@ else
     echo "u r a root user"
 fi
 
-yum install mysql -y
-VALIDATE $? "installation of mysql"
-
-
+yum install myysql -y
