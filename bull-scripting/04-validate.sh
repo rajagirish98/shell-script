fe022be010351961def7c71a913de0a0518e49bd
Girish Raja <you@example.com>
2024-05-06 01:41:14 +0530

    added 2nd script

---
+++ bull-scripting/04-validate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+VALIDATE(){
+    [ ID= -u ]
+if
+    echo ".... Failure $2 "
+else
+    echo ".... Success $0"
+fi
+}
+
+if [ $0 -ne 2 ]
+then
+    echo "failed to install not root user"
+    exit 1
+else
+    echo "success installation"
+fi
+
+VALIDATE( $? )
+yum install mysql -y
