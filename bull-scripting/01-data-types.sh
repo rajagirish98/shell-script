ec6df17cb546835807bb564347a04582f4be3e42
Girish Raja <you@example.com>
2024-05-05 22:27:19 +0530

    added 2nd script

--- bull-scripting/01-data-types.sh
+++ bull-scripting/01-data-types.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-FRIUTS=["Kiwi" "Apple" "Banana" "Apple"]
+FRUITS=["Kiwi" "Apple" "Banana" "Apple"]
 
-echo "First value {$FRIUT1 [$1]}"
+echo "First value {$FRUITS [$1]}"
 
-echo "Second value {$FRIUT2 [$2]}"
+echo "Second value {$FRUITS [$2]}"
 
-echo "Third value {$FRIUT3 [$3]}"
+echo "Third value {$FRUITS [$3]}"
 
-echo "Fourth value {$FRIUT4 [$4]}"
+echo "Fourth value {$FRUITS [$4]}"
