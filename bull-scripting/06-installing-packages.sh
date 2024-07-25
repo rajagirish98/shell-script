289a8bc6a2ed80a71a58209f8caea888b95c6098
Girish Raja <you@example.com>
2024-07-25 16:45:13 +0530

    added 2nd script

--- bull-scripting/06-installing-packages.sh
+++ bull-scripting/06-installing-packages.sh
@@ -4,7 +4,16 @@ ID=$(id -u)
 
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -n 0]
+    then
+        echo -e "$2.... $R FAILED $N"
+    else
+        echo -e "$0.... $G SUCCESS $N"
+}
 
 if [ $ID -ne 0 ]
 then
@@ -15,3 +24,18 @@ else
 fi # fi means reverse of if, indicating condition and
 
 echo "all args passed: $@"
+
+# package=git first time
+
+for package in $@
+do 
+    yum list installed $package
+    if [ $? -ne 0]
+    then
+        yum install $packages -y
+        VALIDATE $? "installation of $package
+    else
+        echo -e "$package is already installed" $Y SKIPPING $N"
+    fi
+done
+    VALIDATE $? "installation of $packages"
